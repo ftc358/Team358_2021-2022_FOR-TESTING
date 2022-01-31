02ddf254689b8ea93935e16aec15a1a27a656879
charliespy <42754940+charliespy@users.noreply.github.com>
2022-01-31 15:56:00 -0800

    push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_SERVO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST_SERVO.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
+@Disabled
 @TeleOp
 public class TEST_SERVO extends LinearOpMode {
 
