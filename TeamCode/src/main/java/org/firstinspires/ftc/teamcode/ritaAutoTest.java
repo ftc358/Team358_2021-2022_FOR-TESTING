dc750cc1c1dd88fee6b1ea2e055ee0b641120ae1
ritawang18 <97565824+ritawang18@users.noreply.github.com>
2022-11-03 15:59:18 -0700

    h

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ritaAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ritaAutoTest.java
@@ -1,9 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
+public class ritaAutoTest extends LinearOpMode {
+    DcMotor leftDrive;
+    DcMotor rightDrive;
 
-public class ritaAutoTest extends RobotMain358 {
+    public void runOpMode() {
 
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+            leftDrive.
+
+        }
+    }
 }
